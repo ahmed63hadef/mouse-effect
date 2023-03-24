e8542dce4ab7d58bd231f6a2f685a063bbe8f4c0
ahmed63hadef <ahmedhadef63@gmail.com>
2023-03-24 22:11:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const mouses = document.querySelectorAll(".mouse");
+window.addEventListener("mousemove", (e) => {
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.y + "px";
+    mouse.style.left = e.x + "px";
+  });
+});
